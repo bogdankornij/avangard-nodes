e91dda65a9dd51c9a5df968a4f09ee33dbdb34e9
bogdankornij <bogdankornij656@gmail.com>
2021-12-01 14:04:58 +0200

    aleo update

---
+++ aleo/update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [ ! -e $HOME/account_aleo.txt ]; then
+  cp $HOME/aleo/account.txt $HOME/account_aleo.txt
+fi
+#add ufw rules
+curl -s https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/ufw.sh | bash
+
+sudo apt install wget -y
+rustup update
+sudo systemctl stop miner
+rm -rf $HOME/snarkOS
+git clone https://github.com/AleoHQ/snarkOS.git --depth 1
+cd $HOME/snarkOS
+cargo build --release --verbose
+
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+sudo systemctl restart systemd-journald
+
+echo 'export MINER_ADDRESS='$(cat $HOME/account_aleo.txt | awk '/Address/ {print $2}') >> $HOME/.profile
+source $HOME/.profile
+sed -i "s/MINER_ADDRESS=\".*\"/MINER_ADDRESS=\"${MINER_ADDRESS}\"/g" $HOME/snarkOS/run-miner.sh
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/miner.service
+[Unit]
+Description=Aleo miner
+After=network-online.target
+[Service]
+Environment=PATH="/root/.cargo/bin/:$PATH"
+Environment=MINER_ADDRESS=$(cat $HOME/account_aleo.txt | awk '/Address/ {print $2}')
+User=$USER
+WorkingDirectory=$HOME/snarkOS
+ExecStart=$HOME/snarkOS/run-miner.sh
+Restart=always
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable miner
+sudo systemctl restart miner
