57a30b26e24a7d1fe49e9f201cb794bbb3411175
bogdankornij <bogdankornij656@gmail.com>
2021-12-02 16:51:00 +0200

    evmos

--- evmos/evmos.sh
+++ evmos/evmos.sh
@@ -11,28 +11,38 @@ sudo apt install nano mc wget -y &>/dev/null
 source .profile
 source .bashrc
 sleep 1
+
+git clone https://github.com/cosmos/cosmos-sdk
+cd cosmos-sdk
+git checkout v0.44.3
+make cosmovisor
+cp cosmovisor/cosmovisor $GOPATH/bin/cosmovisor
+cd $HOME
+
+mkdir -p ~/.evmosd
+mkdir -p ~/.evmosd/cosmovisor
+mkdir -p ~/.evmosd/cosmovisor/genesis
+mkdir -p ~/.evmosd/cosmovisor/genesis/bin
+mkdir -p ~/.evmosd/cosmovisor/upgrades
+
+echo "# Setup Cosmovisor" >> ~/.profile
+echo "export DAEMON_NAME=evmosd" >> ~/.profile
+echo "export DAEMON_HOME=$HOME/.evmosd" >> ~/.profile
+echo 'export PATH="$DAEMON_HOME/cosmovisor/current/bin:$PATH"' >> ~/.profile
+source ~/.profile
+
 if [ ! -d $HOME/evmos/ ]; then
   git clone https://github.com/tharsis/evmos.git &>/dev/null
 	cd $HOME/evmos
-	git checkout v0.1.3 &>/dev/null
+	git checkout v0.3.0 &>/dev/null
 fi
 cd $HOME/evmos
 make install &>/dev/null
-evmosd config chain-id evmos_9000-1 &>/dev/null
+evmosd config chain-id evmos_9000-2 &>/dev/null
 evmosd config keyring-backend file &>/dev/null
-evmosd init "$EVMOS_NODENAME" --chain-id evmos_9000-1 &>/dev/null
-wget -qO $HOME/.evmosd/config/genesis.json https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/genesis.json &>/dev/null
-bootstrap_node="http://5.189.156.65:26657"; \
-latest_height=`wget -qO- "${bootstrap_node}/block" | jq -r ".result.block.header.height"`; \
-block_height=$((latest_height - 2000)); \
-trust_hash=`wget -qO- "${bootstrap_node}/block?height=${block_height}" | jq -r ".result.block_id.hash"`; \
-sed -i -e "s%^moniker *=.*%moniker = \"$EVMOS_NODENAME\"%; "\
-"s%^seeds *=.*%seeds = \"c36cec90ded95d162b85f8ecd00ecd7c8849ca75@arsiamons.seed.evmos.org:26656,`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/seeds.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
-"s%^persistent_peers *=.*%persistent_peers = \"847e72f31e1f87e8059231b4b9e3302989c22d3a@5.189.156.65:26656,`wget -qO - https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/evmos/peers.txt | tr '\n' ',' | sed 's%,$%%'`,`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/peers.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
-"s%^enable *=.*%enable = false%; "\
-"s%^rpc_servers *=.*%rpc_servers = \"${bootstrap_node},${bootstrap_node}\"%; "\
-"s%^trust_height *=.*%trust_height = $block_height%; "\
-"s%^trust_hash *=.*%trust_hash = \"$trust_hash\"%" $HOME/.evmosd/config/config.toml
+evmosd init "$EVMOS_NODENAME" --chain-id evmos_9000-2 &>/dev/null
+curl -s https://raw.githubusercontent.com/tharsis/testnets/main/olympus_mons/genesis.json > ~/.evmosd/config/genesis.json
+
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
 EOF
@@ -55,4 +65,3 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable evmos &>/dev/null
 sudo systemctl start evmos
-echo "Готово, возвращаемся к гайду"
