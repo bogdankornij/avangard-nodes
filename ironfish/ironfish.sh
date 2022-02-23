c6a6ab65f012931b98901c75f27c8abc159ca09b
bogdankornij <bogdankornij656@gmail.com>
2022-02-23 10:47:41 +0200

    ironfish mooving

--- ironfish/ironfish.sh
+++ ironfish/ironfish.sh
@@ -1,6 +1,7 @@
 curl -s https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/docker.sh | bash
-echo "alias ironfish='docker exec ironfish ./bin/run'" >> ~/.bashrc && source ~/.bashrc
-tee <<EOF >/dev/null $HOME/docker-compose.yaml
+echo "alias ironfish='docker exec ironfish ./bin/run'" >> ~/.profile
+source ~/.profile
+sudo tee <<EOF >/dev/null $HOME/docker-compose.yaml
 version: "3.3"
 services:
  ironfish:
@@ -8,7 +9,7 @@ services:
   image: ghcr.io/iron-fish/ironfish:latest
   restart: always
   network_mode: "host"
-  entrypoint: sh -c "apt update > /dev/null && apt install curl -y > /dev/null; ./bin/run start"
+  entrypoint: sh -c "sed -i 's%REQUEST_BLOCKS_PER_MESSAGE.*%REQUEST_BLOCKS_PER_MESSAGE = 5%' /usr/src/app/node_modules/ironfish/src/syncer.ts && apt update > /dev/null && apt install curl -y > /dev/null; ./bin/run start"
   healthcheck:
    test: "curl -s -H 'Connection: Upgrade' -H 'Upgrade: websocket' http://127.0.0.1:9033 || killall5 -9"
    interval: 180s
