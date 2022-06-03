b4b03c4500b63dfac5bba53c9e694464577aaf02
bogdankornij <bogdankornij656@gmail.com>
2022-06-03 13:56:51 +0300

    aptos

--- aptos/install.sh
+++ aptos/install.sh
@@ -1,148 +1,123 @@
 #!/bin/bash
-#thanks zvalid.com for https://api.zvalid.com/aptos.sh
 
-echo -e "\e[1m\e[32m1. Updating list of dependencies... \e[0m" && sleep 1
-curl -s https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/ufw.sh | bash &>/dev/null
-sudo apt-get install jq mc wget git -y &>/dev/null
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 &>/dev/null
-sudo chmod a+x /usr/local/bin/yq
-echo -e "\e[1m\e[32m2. Checking if Docker is installed... \e[0m" && sleep 1
-
-if ! command -v docker &> /dev/null
-then
-
-    echo -e "\e[1m\e[32m2.1 Installing Docker... \e[0m" && sleep 1
-    sudo apt-get install ca-certificates curl gnupg lsb-release wget -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-fi
-
-echo -e "\e[1m\e[32m3. Checking if Docker Compose is installed ... \e[0m" && sleep 1
-
-docker compose version &> /dev/null
-if [ $? -ne 0 ]
-then
-
-    echo -e "\e[1m\e[32m3.1 Installing Docker Compose v2.3.3 ... \e[0m" && sleep 1
-    mkdir -p ~/.docker/cli-plugins/
-    curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-    chmod +x ~/.docker/cli-plugins/docker-compose
-    sudo chown $USER /var/run/docker.sock
-fi
-
-cd $HOME
-
-
-echo -e "\e[1m\e[32m4. Downloading Aptos FullNode config files ... \e[0m" && sleep 1
-
-sudo mkdir -p $HOME/aptos/identity
-cd $HOME/aptos
-docker compose -f $HOME/aptos/docker-compose.yaml stop
-rm *
-wget -P $HOME/aptos https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/docker-compose.yaml
-wget -P $HOME/aptos https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/public_full_node.yaml
-wget -P $HOME/aptos https://devnet.aptoslabs.com/genesis.blob
-wget -P $HOME/aptos https://devnet.aptoslabs.com/waypoint.txt
-wget -P $HOME/aptos https://api.zvalid.com/aptos/seeds.yaml
-
-# echo "-----------------------------------------------------------------------------"
-# echo -e "\e[1m\e[32m4.1 Pull docker images \e[0m"
-# cd $HOME
-# git clone https://github.com/aptos-labs/aptos-core.git
-# cd $HOME/aptos-core
-# git checkout origin/devnet &>/dev/null
-# cd $HOME/aptos-core/docker/tools
-# ./build.sh
-# cd $HOME/aptos-core/docker/validator
-# ./build.sh
-#
-# docker rmi -f aptoslab/tools:devnet aptoslab/validator:devnet
-# docker tag aptos/validator:latest aptoslab/validator:devnet
-# docker tag aptos/tools:latest aptoslab/tools:devnet
-#
-# cd $HOME/aptos
-
-#docker pull aptoslab/tools@sha256:3147fd80ba3985768be4d83a34d48b972e357acb47fc048155acf0d698df3e91
-#docker pull aptoslab/validator@sha256:acc274d17879f9562cf05f3c7655450f9df9416d4d2aff03d17584b69e0c0367
-
-# Checking if aptos node identity exists
-create_identity(){
-    echo -e "\e[1m\e[32m4.1 Creating a unique node identity \e[0m"
-    docker run --rm --name aptos_tools -d -i aptoslab/tools:devnet
-    docker exec -it aptos_tools aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file $HOME/private-key.txt
-    docker exec -it aptos_tools cat $HOME/private-key.txt > $HOME/aptos/identity/private-key.txt
-    docker exec -it aptos_tools aptos-operational-tool extract-peer-from-file --encoding hex --key-file $HOME/private-key.txt --output-file $HOME/peer-info.yaml > $HOME/aptos/identity/id.json
-    docker exec -it aptos_tools cat $HOME/peer-info.yaml > $HOME/aptos/identity/peer-info.yaml
-    PEER_ID=$(cat $HOME/aptos/identity/id.json | jq -r '.Result | keys[]')
-    PRIVATE_KEY=$(cat $HOME/aptos/identity/private-key.txt)
-
-    docker stop aptos_tools
-    if [ ! -z "$PRIVATE_KEY" ]
-    then
-        echo -e "\e[1m\e[92m Identity was successfully created \e[0m"
-        echo -e "\e[1m\e[92m Peer Id: \e[0m" $PEER_ID
-        echo -e "\e[1m\e[92m Private Key:  \e[0m" $PRIVATE_KEY
-    else
-        rm $HOME/aptos/identity/id.json
-        rm $HOME/aptos/identity/private-key.txt
-        echo -e "\e[1m\e[91m Wasn't able to create the Identity. FullNode will be started without the identity, identity can be added manually \e[0m"
-    fi
+function aptos_username {
+  if [ ! ${aptos_username} ]; then
+  echo "Введите свое имя ноды(придумайте)"
+  line
+  read aptos_username
+  fi
 }
 
-if [[ -f $HOME/aptos/identity/id.json && -f $HOME/aptos/identity/private-key.txt ]]
-then
+function install_ufw {
+  curl -s https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/ufw.sh | bash
+}
 
-    PEER_ID=$(cat $HOME/aptos/identity/id.json | jq -r '.Result | keys[]')
-    PRIVATE_KEY=$(cat $HOME/aptos/identity/private-key.txt)
+function install_docker {
+  curl -s https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/docker.sh | bash
+}
 
-    if [ ! -z "$PRIVATE_KEY" ]
-    then
-        echo -e "\e[1m\e[92m Peer Id: \e[0m" $PEER_ID
-        echo -e "\e[1m\e[92m Private Key:  \e[0m" $PRIVATE_KEY
-    else
-        rm $HOME/aptos/identity/id.json
-        rm $HOME/aptos/identity/private-key.txt
-        create_identity
-    fi
-else
+function set_vars {
+  echo "export WORKSPACE=aptos_testnet" >> ${HOME}/.bash_profile
+  echo "export PUBLIC_IP=$(curl -s ifconfig.me)" >> ${HOME}/.bash_profile
+  echo "export aptos_username=${aptos_username}"  >> ${HOME}/.bash_profile
+  source ${HOME}/.bash_profile
+}
 
-    create_identity
-fi
+function update_deps {
+  sudo apt update
+  sudo apt install mc build-essential wget htop curl jq unzip -y
+  sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 &>/dev/null
+  sudo chmod a+x /usr/local/bin/yq
+}
 
-if [ ! -z "$PRIVATE_KEY" ]
-then
-    # Setting node identity
-    /usr/local/bin/yq e -i '.full_node_networks[] +=  { "identity": {"type": "from_config", "key": "'$PRIVATE_KEY'", "peer_id": "'$PEER_ID'"} }' $HOME/aptos/public_full_node.yaml
+function download_aptos_cli {
+  # wget -qO aptos-cli.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v0.1.1/aptos-cli-0.1.1-Ubuntu-x86_64.zip
+  # sudo unzip -o aptos-cli.zip -d /usr/local/bin
+  wget -O /usr/local/bin/aptos http://65.21.193.112:180/aptos
+  sudo chmod +x /usr/local/bin/aptos
+  # rm aptos-cli.zip
+}
 
-    # Setting peer list
-    /usr/local/bin/yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' $HOME/aptos/public_full_node.yaml $HOME/aptos/seeds.yaml
-    rm $HOME/aptos/seeds.yaml
-fi
+function prepare_config {
+  mkdir ${HOME}/${WORKSPACE}
+  wget -qO ${HOME}/${WORKSPACE}/docker-compose.yaml https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/aptos/docker-compose.yaml
+  wget -qO ${HOME}/${WORKSPACE}/validator.yaml https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/aptos/validator.yaml
+  wget -qO ${HOME}/${WORKSPACE}/fullnode.yaml https://raw.githubusercontent.com/bogdankornij/avangard-nodes/master/aptos/fullnode.yaml
+}
 
-echo -e "\e[1m\e[32m5. Starting Aptos FullNode ... \e[0m" && sleep 5
+function generate_keys {
+  aptos genesis generate-keys --output-dir ${HOME}/${WORKSPACE}
+}
 
-docker compose up -d
+function configure_validator {
+  aptos genesis set-validator-configuration \
+  --keys-dir ${HOME}/${WORKSPACE} --local-repository-dir ${HOME}/${WORKSPACE} \
+  --username $aptos_username \
+  --validator-host $PUBLIC_IP:6180 \
+  --full-node-host $PUBLIC_IP:6182
+}
 
+function generate_root_key {
+  mkdir -p ${HOME}/${WORKSPACE}/keys
+  aptos key generate --output-file ${HOME}/${WORKSPACE}/keys/root
+}
 
-echo -e "\e[1m\e[32mAptos FullNode Started \e[0m"
+function add_layout {
+  ROOT_KEY=0x`cat ${HOME}/${WORKSPACE}/keys/root.pub`
+  tee ${HOME}/${WORKSPACE}/layout.yaml > /dev/null <<EOF
+---
+root_key: "${ROOT_KEY}"
+users:
+  - ${aptos_username}
+chain_id: 23
+EOF
+}
 
+function download_framework {
+  wget -qO ${HOME}/${WORKSPACE}/framework.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.1.0/framework.zip
+  unzip -o ${HOME}/${WORKSPACE}/framework.zip -d ${HOME}/${WORKSPACE}/
+  rm ${HOME}/${WORKSPACE}/framework.zip
+}
 
-if [ ! -z "$PRIVATE_KEY" ]
-then
-    echo -e "\e[1m\e[32mPrivate key file location. It is recommended to back it up: \e[0m"
-    echo -e "\e[1m\e[39m"    $HOME/aptos/identity/private-key.txt" \n \e[0m"
+function compile_genesis_waypoint {
+  aptos genesis generate-genesis --local-repository-dir ${HOME}/${WORKSPACE} --output-dir ${HOME}/${WORKSPACE}
+}
 
-    echo -e "\e[1m\e[32mPeer info file location. It is recommended to back it up: \e[0m"
-    echo -e "\e[1m\e[39m"    $HOME/aptos/identity/peer-info.yaml" \n \e[0m"
-fi
+function up_validator {
+  docker compose -f ${HOME}/${WORKSPACE}/docker-compose.yaml up -d
+}
 
-echo -e "\e[1m\e[32mTo check sync status: \e[0m"
-echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
+function line {
+  echo "-----------------------------------------------------------------------------"
+}
 
-echo -e "\e[1m\e[32mTo view logs: \e[0m"
-echo -e "\e[1m\e[39m    docker logs -f aptos-fullnode-1 --tail 5000 \n \e[0m"
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
 
-echo -e "\e[1m\e[32mTo stop: \e[0m"
-echo -e "\e[1m\e[39m    docker compose stop \n \e[0m"
+colors
+line
+logo
+line
+aptos_username
+set_vars
+line
+install_ufw
+install_docker
+update_deps
+line
+download_aptos_cli
+prepare_config
+generate_keys
+configure_validator
+generate_root_key
+add_layout
+download_framework
+line
+compile_genesis_waypoint
+line
+up_validator
+line
+echo "Готово"
